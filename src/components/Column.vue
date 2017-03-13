27ee79e91fb9cdb723c7281b54d66bafa92d07a6
zhouxinnana <609582047@qq.com>
2017-03-13 21:37:41 +0800

    test swiper

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,37 +1,24 @@
 <template>
     <div class="content">
-        <div class="newsList mt">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+                <div class="swiper-slide">Slide 4</div>
+                <div class="swiper-slide">Slide 5</div>
+                <div class="swiper-slide">Slide 6</div>
+                <div class="swiper-slide">Slide 8</div>
+                <div class="swiper-slide">Slide 9</div>
+                <div class="swiper-slide">Slide 10</div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+        <div class="newsList ">
             <ul>
                 <li>
                     <h2>栏目</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
-                    <p>asdsad</p>
-                </li>
-                <li>
-                    <h2>asdsadsad</h2>
                     <p>asdsad</p>
                 </li>
             </ul>
@@ -40,11 +27,52 @@
 </template>
 
 <script>
+    export default {
+        mounted(){
+            this.renderFn();
+        },
+        methods: {
+            renderFn() {
+                var swiper = new Swiper('.swiper-container', {
+                    slidesPerView: 4,
+                    paginationClickable: true,
+                    spaceBetween: 5,
+                    freeMode: true
+                });
+                alert(123);
+            }
+        }
+    }
 </script>
 
 
 <style scoped>
-    .mt{
-        margin-top:40px; 
+    .mt {
+        margin-top: 40px;
+    }
+
+    .swiper-container {
+        width: 100%;
+        height:0.5rem;
+        margin: 40px auto;
+    }
+
+    .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
     }
 </style>
